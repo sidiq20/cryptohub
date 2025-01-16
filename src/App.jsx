8339a5f0410b654caa271a01361bb2d2a0105b40
sidiq20 <sidiqolasode@gmail.com>
2025-01-16 17:57:19 +0100

    secound commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
+import HomePage from './pages/Homepage';
 import AdminPage from './pages/AdminPage';
 import WaitlistPage from './pages/WaitlistPage';
 
